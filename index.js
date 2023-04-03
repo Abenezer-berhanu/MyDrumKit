6b7de18d7ed226f9da1c9c763abf779ff9de880b
Abenezer Berhanu <abenezerberhanu40@gmail.com>
2023-04-03 11:26:32 -0400

    create music and images folders

--- index.js
+++ index.js
@@ -14,31 +14,31 @@ document.addEventListener("keypress", function (event) {
 function makeSound(value) {
   switch (value) {
     case "w":
-      var voice1 = new Audio("alan.m4a");
+      var voice1 = new Audio("./music/alan.m4a");
       voice1.play();
       break;
     case "a":
-      var voice2 = new Audio("alok.m4a");
+      var voice2 = new Audio("./music/alok.m4a");
       voice2.play();
       break;
     case "s":
-      var voice3 = new Audio("bnxn.m4a");
+      var voice3 = new Audio("./music/bnxn.m4a");
       voice3.play();
       break;
     case "d":
-      var voice4 = new Audio("fireboy.m4a");
+      var voice4 = new Audio("./music/fireboy.m4a");
       voice4.play();
       break;
     case "j":
-      var voice5 = new Audio("justin.m4a");
+      var voice5 = new Audio("./music/justin.m4a");
       voice5.play();
       break;
     case "k":
-      var voice6 = new Audio("libiance.m4a");
+      var voice6 = new Audio("./music/libiance.m4a");
       voice6.play();
       break;
     case "l":
-      var voice7 = new Audio("alan.m4a");
+      var voice7 = new Audio("./music/alan.m4a");
       voice7.play();
       break;
     default:
